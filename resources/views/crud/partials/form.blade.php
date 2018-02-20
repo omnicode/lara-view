471023d073fdd16d65dc80e4b26dd894bea1cb22
davitbek <davitzeynalyan1991@gmail.com>
2018-02-20 14:14:15 +0400

    improve

--- resources/views/crud/partials/form.blade.php
+++ resources/views/crud/partials/form.blade.php
@@ -2,25 +2,7 @@
 @if(view()->exists('lara-view::layouts.app11')))
 
 @else
-    <form action="/action_page.php">
-        @foreach (\Illuminate\Support\Facades\Schema::getColumnListing('currencies') as $column)
-            @if (!in_array($column, ['id', 'created_at', 'updated_at']))
-                @php
-                    $type = \Illuminate\Support\Facades\Schema::getColumnType('currencies', $column);
-                    $arr = [
-                        'boolean' => 'checkbox'
-                    ];
-                    $type = !empty($arr[$type]) ? $arr[$type] : $type;
-                @endphp
-                <div class="form-group">
-                    <label for="email"> {{ ucfirst($column) }}</label>
-                    <input type="{{ $type }}" class="form-control" id="email">
-                </div>
-
-            @endif
-        @endforeach
-        <button type="submit" class="btn btn-default">Submit</button>
-    </form>
+    @include('lara-view::sections.crud.partials.form')
 @endif
 
 
