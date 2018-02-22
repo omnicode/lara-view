ff293110eb2ebfc5e4a1d4206584a6b85c9a77a5
davitbek <davitzeynalyan1991@gmail.com>
2018-02-22 14:46:02 +0400

    version 2.0.0

--- resources/views/sections/crud/partials/form.blade.php
+++ resources/views/sections/crud/partials/form.blade.php
@@ -9,28 +9,62 @@
         $table = isset($item) ? $item->getTable() : str_slug($itemName, '_');
     @endphp
 
-
-    {!! LaraForm::create((isset($item) ? $item : null), ['action' => $route]) !!}
-
-    @foreach (\Illuminate\Support\Facades\Schema::getColumnListing($table) as $column)
+    {!! LaraForm::create((isset($item) ? $item : null), ['url' => $route]) !!}
+    @foreach (LaraDB::getColumnsFullInfo($table) as $column => $info)
         @if (!in_array($column, ['id', 'created_at', 'updated_at']))
             @php
-                $type = \Illuminate\Support\Facades\Schema::getColumnType($table, $column);
+                $data = [];
+                $type = $info['type'];
                 $arr = [
-                    'boolean' => 'checkbox'
+                    'boolean' => 'checkbox',
+                    'int' => 'number',
+                    'smallint' => 'number',
+                    'tinyint' => 'number',
+                    'varchar' => 'text',
+                    'char' => 'text'
                 ];
-                $type = !empty($arr[$type]) ? $arr[$type] : $type;
-                //echo $type;
+
+                if ($column == 'email') {
+                    $type = 'email';
+                } elseif ($column == 'password') {
+                    $type = 'password';
+                } elseif (starts_with($column, 'is')) {
+                    $type = 'checkbox';
+                } elseif (ends_with($column, '_id')) {
+                    $_table = \LaraSupport\Str::before($column, '_id');
+                    $_table = str_plural($_table);
+                    if(!empty(LaraDB::getDBStructure()[$_table])) {
+                        $all = DB::table($_table)->get();
+                        $data = [];
+                        foreach ($all as $instance)
+                        {
+                            $vars = get_object_vars($instance);
+                            $options[$vars['id']] = implode(',', $vars);
+                        }
+                        $data['options'] = $options;
+                    }
+                    $type = 'select';
+                } elseif ($type === false) {
+                    if ($info['full_info'] === 'datetime') {
+                        $type = 'date';
+                    } elseif($info['full_info'] === 'text') {
+                        $type = 'textarea';
+                    }
+                }elseif (!empty($arr[$type]) ) {
+                    $type = $arr[$type];
+                } else {
+                //TODO
+                    $type = 'text';
+                }
+                $data['type'] = $type;
+
+
+
             @endphp
 
             <div class="row">
                 <div class="col-sm-12">
-                @if ($type == 'checkbox')
-                    {!! LaraForm::checkbox($column) !!}
-                @else
-                    {!! LaraForm::input($column) !!}
-                @endif
-
+                    {!! LaraForm::input($column, $data) !!}
                 </div>
             </div>
         @endif
