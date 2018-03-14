2f6cc65a97977c4103d2ee34237a4e0c181798c8
davitbek <davitzeynalyan1991@gmail.com>
2018-03-15 00:59:10 +0400

    Update lara_view.php

--- config/lara_view.php
+++ config/lara_view.php
@@ -1,26 +1,5 @@
 <?php
 return [
-    'theme' => 'https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip',
-    'css' => [
-        'app.css',
-        'core.min.css',
-        'bootstrap/css/bootstrap.min.css',
-        'template/metisMenu/metisMenu.min.css',
-        'template/sb-admin/sb-admin-2.min.css',
-        'font-awesome/css/font-awesome.min.css',
-        'main.css'
-    ],
-
-    'js' => [
-        'app.js',
-        'core.js',
-        'jquery.min.js',
-        'bootstrap/js/bootstrap.min.js',
-        'main.js',
-        'template/metisMenu/metisMenu.min.js',
-        'template/sb-admin/sb-admin-2.min.js',
-    ],
-
     'show' => [
         'column_list' => 'list',
     ],
