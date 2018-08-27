fbe81a3b8b85e8b0cf9ac0a5805027f6a5970b77
davitbek <davitzeynalyan1991@gmail.com>
2018-08-27 19:53:49 +0400

    Update index.blade.php

--- resources/views/sections/crud/index.blade.php
+++ resources/views/sections/crud/index.blade.php
@@ -1,24 +1,28 @@
-
 @php
-    if (empty($pattern)) {
-        if (!empty($columns['table'])) {
-            $pattern = $columns['table'];
-        } else {
-            throw new Exception('lara-view::index must be pass pattern variable for showing title, empty result');
-        }
-    }
+   if (empty($pattern)) {
+       if (!empty($columns['table'])) {
+           $pattern = $columns['table'];
+       } else {
+           throw new Exception('lara-view::index must be pass pattern variable for showing title, empty result');
+       }
+   }
 
-    $preRoute = str_replace_last('index', '', Route::currentRouteName());
-    $name = str_replace('_', ' ', title_case($pattern));
+   $preRoute = str_replace_last('index', '', Route::currentRouteName());
+   $name = str_replace('_', ' ', title_case($pattern));
+   $_add = isset($_add) ? $_add : true;
+   $_title = isset($_title) ? $_title : true;
 @endphp
 
-@include('lara-view::sections.title', ['_title' => $name])
-
-@include('lara-view::sections.add', ['_route' => $preRoute . 'create', '_name' => 'Add ' . str_singular($name)])
+@if($_title)
+   @include('lara-view::sections.title', ['_title' => $name])
+@endif
+@if($_add)
+   @include('lara-view::sections.add', ['_route' => $preRoute . 'create', '_name' => 'Add ' . str_singular($name)])
+@endif
 
 @if(count($items) > 0)
-    @include('lara-view::sections.crud.index._index', ['_items' => $items])
-    @include('lara-view::sections.crud.index.pagination', ['_items' => $items])
+   @include('lara-view::sections.crud.index._index', ['_items' => $items])
+   @include('lara-view::sections.crud.index.pagination', ['_items' => $items])
 @else
-    @include('lara-view::sections.empty-result', ['_info' => 'No ' . $name])
+   @include('lara-view::sections.empty-result', ['_info' => 'No ' . $name])
 @endif
